a3e906eb6a651e6f350a4638f5c4efb1e385b255
arnav gupta <77843522+arnavg11@users.noreply.github.com>
2021-03-10 18:36:56 +0530

    Add files via upload

--- noise sketches/terrain generation using perlin/sketch.js
+++ noise sketches/terrain generation using perlin/sketch.js
@@ -1,4 +1,4 @@
-var terrain = [], yoff = 100, p
+var terrain = [], yoff = 100
 const scl = 10, w = 500, h = 500
 function setup() {
   createCanvas(400, 400,WEBGL);
@@ -13,32 +13,34 @@ function setup() {
     }
     yoff-=.04
   }
-  // noFill()
-  fill(51)
-  p = createSlider(0,PI/2, 1, .1)
+  noFill()
 }
 
 function draw() {
   background(51);
   // translate(width/2,height/2);
-  rotateX(p.value());
+  rotateX(1);
+  noStroke()
   push()
   translate(-w/2, -h/2+100)
   for(let y = scl; y<h; y+=scl){
+    // fill(255)
     beginShape(TRIANGLE_STRIP)
     for(let x = 0; x<w; x+=scl){
       let x_ = x/scl, y_ = y/scl
+      // fill(map(terrain[y_][x_],0,200,0,300))
+      fill(noise(terrain[y_][x_],0)*255, noise(terrain[y_][x_],100)*255, noise(terrain[y_][x_],200)*255)
       vertex(x,y,terrain[y_][x_]); 
       vertex(x,(y-scl),terrain[y_-1][x_])
     }
     endShape()
   }
-  for(let i = 1; i<w/scl; i++){
-    line(i*scl,0,terrain[0][i],i*scl-scl,0,terrain[0][i-1])
-  }
-  for(let i = 1; i<h/scl; i++){
-    line(w-scl,i*scl,terrain[i][w/scl-1],w-scl,i*scl-scl,terrain[i-1][w/scl-1])
-  }
+  // for(let i = 1; i<w/scl; i++){
+  //   line(i*scl,0,terrain[0][i],i*scl-scl,0,terrain[0][i-1])
+  // }
+  // for(let i = 1; i<h/scl; i++){
+  //   line(w-scl,i*scl,terrain[i][w/scl-1],w-scl,i*scl-scl,terrain[i-1][w/scl-1])
+  // }
   pop()
   // add new row
   terrain.unshift([])
@@ -49,7 +51,4 @@ function draw() {
     xoff-=.04
   }
   yoff-=.04
-}
-function keyPressed(){
-  console.log(p.value())
 }
